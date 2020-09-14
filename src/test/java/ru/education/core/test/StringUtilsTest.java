781ec3d06f44b71a6055d60d065c6008b90d5e32
Evgeniy <solodimov.eugene@yandex.ru>
2020-09-14 14:45:32 +0300

    init for test

---
+++ src/test/java/ru/education/core/test/StringUtilsTest.java
@@ -0,0 +1,36 @@
+package ru.education.core.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+import ru.education.core.util.StringUtils;
+
+public class StringUtilsTest {
+
+    @Test
+    public void isEmptyReturnTrueWhenStringIsNull() {
+        String input = null;
+
+        boolean isEmpty = StringUtils.isEmpty(input);
+
+        Assert.assertEquals(true, isEmpty);
+    }
+
+    @Test
+    public void isEmptyReturnTrueWhenStringIsEmpty() {
+        String input = "      ";
+
+        boolean isEmpty = StringUtils.isEmpty(input);
+
+        Assert.assertEquals(true, isEmpty);
+    }
+
+    @Test
+    public void isEmptyReturnFalseWhenStringIsNotEmpty() {
+        String input = "not empty";
+
+        boolean isEmpty = StringUtils.isEmpty(input);
+
+        Assert.assertEquals(false, isEmpty);
+    }
+
+}
